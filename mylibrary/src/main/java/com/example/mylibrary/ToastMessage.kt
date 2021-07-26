87191e87d7aff5ae261ee786207721e861e5875f
Thaw Zin Aung <thawzinaung.tza94@gmail.com>
2021-07-26 10:51:14 +0630

    Modify show toast mesage

--- mylibrary/src/main/java/com/example/mylibrary/ToastMessage.kt
+++ mylibrary/src/main/java/com/example/mylibrary/ToastMessage.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.widget.Toast
 
 class ToastMessage {
-    fun toastMessage(c: Context, message: String) {
-        Toast.makeText(c, message, Toast.LENGTH_LONG).show()
+    companion object {
+        fun show(c: Context, message: String) {
+            Toast.makeText(c, message, Toast.LENGTH_LONG).show()
+        }
     }
 }
