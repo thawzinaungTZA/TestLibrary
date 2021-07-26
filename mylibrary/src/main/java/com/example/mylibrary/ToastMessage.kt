b259a98804e989e3f2a0a21be28aef4f7abea62e
Thaw Zin Aung <thawzinaung.tza94@gmail.com>
2021-07-26 10:36:16 +0630

    Fix toastmessage

--- mylibrary/src/main/java/com/example/mylibrary/ToastMessage.kt
+++ mylibrary/src/main/java/com/example/mylibrary/ToastMessage.kt
@@ -3,7 +3,7 @@ package com.example.mylibrary
 import android.content.Context
 import android.widget.Toast
 
-object ToastMessage {
+class ToastMessage {
     fun toastMessage(c: Context, message: String) {
         Toast.makeText(c, message, Toast.LENGTH_LONG).show()
     }
